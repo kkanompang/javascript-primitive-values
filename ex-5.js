a31d9afad2202ac8300f4bcb4cf7a9b3a6db9ee1
Kanompang <suparadajk21@gmail.com>
2024-08-31 21:33:25 +0700

    BP-5-Kanompang

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let shoppingValue = 27000;
+let discount = (shoppingValue*25/100);
+let amount = (shoppingValue - discount);
+console.log(amount)
